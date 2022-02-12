993e1ab774a0b9ed6c247fa8a77157b0fede046c
Victor Merino <victor.merinogarcia@gmail.com>
2022-02-12 23:55:49 +0100

    Add service to call to spotify search, previously requesting access token to the backend /register-spotify-app endpoint

--- services/queryService.ts
+++ services/queryService.ts
@@ -1,4 +1,33 @@
 export const getResultsFromQuery = async (text: string) => {
-  console.log(text)
-  return []
+  let resultToSend = {}
+  const json = await _requestAccessToken()
+  const token = json.token
+
+  const searchUrl = 'https://api.spotify.com/v1/search'
+  const searchType = 'album,artist,track'
+  const getResultsEndpoint = `${searchUrl}?type=${searchType}&q=${text}`
+
+  try {
+    const response = await fetch(getResultsEndpoint, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + token
+      }
+    })
+    const result = await response.json()
+    resultToSend = result
+    return result
+  } catch (error) {
+    console.log(error)
+  }
+  return resultToSend
+}
+
+const _requestAccessToken = async () => {
+  return await fetch('http://localhost:3030/register-spotify-app')
+    .then(result => {
+      console.log('line 31', result)
+      return result.json()
+    })
+    .catch(error => console.log(error))
 }
