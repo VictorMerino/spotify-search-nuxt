7ae6546ef89701a9336db5fb77c97b4856321235
Victor Merino <victor.merinogarcia@gmail.com>
2022-02-12 17:41:18 +0100

    Add SearchBox

---
+++ components/SearchBox.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+const emit = defineEmits<{
+  (e: 'searchText', text: string): void
+}>()
+
+const searchField = ref('')
+function searchText() {
+  emit('searchText', searchField.value)
+}
+</script>
+<template>
+  <section class="search-box my-4 p-4">
+    <h1 data-testid="app-heading" class="mb-2 text-xl">
+      Spotify Tracks Search
+    </h1>
+    <form role="search" class="search-wrapper flex">
+      <input
+        type="text"
+        name="search"
+        id="search"
+        v-model="searchField"
+        data-testid="search-input"
+        placeholder="Search for a track, artist and/or album"
+        class="search-input border border-slate-400 rounded-l-xl py-2 px-6 flex-1 focus-visible:outline-none" />
+      <button
+        class="search-btn rounded-r-xl p-4 bg-slate-300"
+        data-testid="search-btn"
+        @click.prevent="searchText">
+        Search
+      </button>
+    </form>
+  </section>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,13 @@
+<script setup lang="ts">
+import SearchBox from '@/components/SearchBox.vue'
+
+const searchText = (text: string) => {
+  console.log(text)
+}
+</script>
 <template>
   <main>
-    <p>Searchbox</p>
+    <SearchBox @search-text="searchText" />
     <p>ResultList</p>
   </main>
 </template>
