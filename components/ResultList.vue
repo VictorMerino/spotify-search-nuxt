3d9c1d7ef99765d7848b1303fb57a9dc0fd94d46
Victor Merino <victor.merinogarcia@gmail.com>
2022-02-12 18:06:13 +0100

    Add ResultList component

---
+++ components/ResultList.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+const props = defineProps<{ resultRaw }>()
+</script>
+
+<template>
+  <section class="result-list container">
+    <template v-if="Object.entries(resultRaw).length">
+      <template v-for="(resultType, name, index) in resultRaw" :key="index">
+        <h2>{{ name }}</h2>
+        <hr />
+        <div
+          class="grid-10"
+          data-testid="results-wrapper"
+          v-if="Object.entries(resultType.items).length">
+          Result {{ resultType }}
+        </div>
+        <div v-else data-testid="no-result-msg">No {{ name }} found</div>
+      </template>
+    </template>
+    <template v-else>
+      Please, use the above input to search for any result
+    </template>
+  </section>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
 import SearchBox from '@/components/SearchBox.vue'
+import ResultList from '@/components/ResultList.vue'
 
+const isLoading = ref(false)
 const searchText = (text: string) => {
   console.log(text)
 }
+const resultList = computed(() => [])
 </script>
 <template>
   <SearchBox @search-text="searchText" />
-  <p>ResultList</p>
+  <div v-if="isLoading" class="loading"></div>
+  <ResultList v-else :result-raw="resultList" />
 </template>
