e3ef5bcae2527f35e6250e73f23c846570cb6db1
Victor Merino <victor.merinogarcia@gmail.com>
2022-02-12 16:10:07 +0100

    Add basic default layout: here is were we will put all main template stuff: metatags, header, footer... Is like the base-template in django

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<script setup>
+const title = ref('VIC Spotify Search App')
+</script>
+
+<template>
+  <div>
+    <Html lang="en">
+      <Head>
+        <Title>{{ title }}</Title>
+        <Meta
+          name="description"
+          :content="`App trying Nuxt 3 with Spotify Search API`" />
+      </Head>
+    </Html>
+
+    HEADER
+    <slot />
+
+    FOOTER
+  </div>
+</template>
