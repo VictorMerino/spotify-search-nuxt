30a63fca896542419f13728431f49ee1fd4f8b6f
Victor Merino <victor.merinogarcia@gmail.com>
2022-02-13 15:11:33 +0100

    Just little layout adjustment for SearchBox: text clarity and borders

--- components/SearchBox.vue
+++ components/SearchBox.vue
@@ -10,9 +10,6 @@ function searchText() {
 </script>
 <template>
   <section class="search-box my-4 p-4">
-    <h1 data-testid="app-heading" class="mb-2 text-xl">
-      Spotify Tracks Search
-    </h1>
     <form role="search" class="search-wrapper flex">
       <input
         type="text"
@@ -21,9 +18,9 @@ function searchText() {
         v-model="searchField"
         data-testid="search-input"
         placeholder="Search for a track, artist and/or album"
-        class="search-input border border-slate-400 rounded-l-full py-2 px-6 flex-1 text-slate-800 focus-visible:outline-none" />
+        class="search-input rounded-l-full py-2 px-6 flex-1 text-slate-800 focus-visible:outline-none" />
       <button
-        class="search-btn rounded-r-full p-4 bg-slate-300"
+        class="search-btn rounded-r-full p-4 bg-slate-300 text-slate-800"
         data-testid="search-btn"
         @click.prevent="searchText">
         Search
