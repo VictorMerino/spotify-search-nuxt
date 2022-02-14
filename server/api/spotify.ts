b60befa3799fa1f397d31e551b2ecf499d9f7683
Victor Merino <victor.merinogarcia@gmail.com>
2022-02-15 00:21:09 +0100

    Simple nitro api endpoint: get the token and return it to queryService

--- server/api/spotify.ts
+++ server/api/spotify.ts
@@ -1,10 +1,27 @@
+import requestPromise from 'request-promise'
+
 const clientId = process.env.CLIENT_ID
 const clientSecret = process.env.CLIENT_SECRET
 const spotifyGetTokenURL = process.env.ACCOUNT_TOKEN_URL
 
-export default async (req, res) => {
-  console.log(clientId)
-  console.log(clientSecret)
-  console.log(spotifyGetTokenURL)
-  return 'Spotify'
+export default async () => {
+  const authOptions = {
+    url: spotifyGetTokenURL,
+    headers: {
+      Authorization:
+        'Basic ' + Buffer.from(clientId + ':' + clientSecret).toString('base64')
+    },
+    form: {
+      grant_type: 'client_credentials'
+    },
+    json: true
+  }
+
+  let token: string
+
+  await requestPromise.post(authOptions, function (error, response, body) {
+    if (error) console.log(error)
+    token = body.access_token
+  })
+  return { token }
 }
