9e311f5ddd2c7019a26ef06f796525efd20c7363
Victor Merino <victor.merinogarcia@gmail.com>
2022-02-15 01:19:54 +0100

    Fix header img

--- components/structure/Header.vue
+++ components/structure/Header.vue
@@ -3,7 +3,7 @@
     <nav class="p-4 bg-black">
       <div class="container flex justify-between items-center">
         <span class="text-xl">Spotify Search App</span>
-        <img class="h-8" alt="Spotify logo" src="spotify_logo.png" />
+        <img class="h-8" alt="Spotify logo" src="/spotify_logo.png" />
       </div>
     </nav>
   </header>
