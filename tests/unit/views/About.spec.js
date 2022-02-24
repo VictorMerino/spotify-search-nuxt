073310397aefc11189efe9e3ee49eb986d0c98b8
Victor Merino <victor.merinogarcia@gmail.com>
2022-02-24 21:32:58 +0100

    About spec cleanup

--- tests/unit/views/About.spec.js
+++ tests/unit/views/About.spec.js
@@ -1,49 +1,17 @@
-import { screen, render } from '@testing-library/vue'
-import userEvent from '@testing-library/user-event'
-import App from '@/pages/index.vue'
-import { describe, expect, vi } from 'vitest'
+import { render, fireEvent } from '@testing-library/vue'
+import About from '@/pages/about/index.vue'
+import { describe, expect } from 'vitest'
 
-// process.env.ENV_VARIABLE = ''
-const customRender = () => {
-  const storeObj = () => ({
-    modules: {
-      // exampleStore: cloneDeep(exampleStore)
-    }
-  })
-  return render(App, {
-    global: {
-      // plugins: [createStore(storeObj()), router]
-    }
-  })
-}
-
-global.fetch = vi.fn(id =>
-  Promise.resolve({
-    json: () =>
-      Promise.resolve({
-        id: 1,
-        name: `bulbasaur${id}`,
-        order: 1,
-        types: [
-          {
-            type: {
-              name: 'grass'
-            }
-          },
-          {
-            type: {
-              name: 'poison'
-            }
-          }
-        ],
-        sprites: {
-          front_default:
-            'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png'
-        }
-      })
-  })
-)
-
-describe('First test', () => {
+describe('About page tests', () => {
   expect(true).toBe(true)
+  it('shows', async () => {
+    const ELEMENT_TEXT = 'About'
+
+    const { getByText } = render(About)
+    const mainDiv = getByText(ELEMENT_TEXT)
+
+    await fireEvent.click(mainDiv)
+
+    getByText(ELEMENT_TEXT)
+  })
 })
