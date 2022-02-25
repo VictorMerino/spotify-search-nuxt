b03f332c133adf6a22ce62056548bddef0a0e040
Victor Merino <victor.merinogarcia@gmail.com>
2022-02-25 19:56:05 +0100

    Add results mock to be used in searchText mock

---
+++ tests/unit/mocks/results.js
@@ -0,0 +1,352 @@
+export const albumMock = {
+  album_type: 'album',
+  artists: [
+    {
+      external_urls: {
+        spotify: 'https://open.spotify.com/artist/0oSGxfWSnnOXhD2fKuz2Gy'
+      },
+      href: 'https://api.spotify.com/v1/artists/0oSGxfWSnnOXhD2fKuz2Gy',
+      id: '0oSGxfWSnnOXhD2fKuz2Gy',
+      name: 'David Bowie',
+      type: 'artist',
+      uri: 'spotify:artist:0oSGxfWSnnOXhD2fKuz2Gy'
+    }
+  ],
+  available_markets: [
+    'AD',
+    'AE',
+    'AG',
+    'AL',
+    'AM',
+    'AO',
+    'AR',
+    'AT',
+    'AU',
+    'AZ',
+    'BA',
+    'BB',
+    'BD',
+    'BE',
+    'BF',
+    'BG',
+    'BH',
+    'BI',
+    'BJ',
+    'BN',
+    'BO',
+    'BR',
+    'BS',
+    'BT',
+    'BW',
+    'BY',
+    'BZ',
+    'CA',
+    'CD',
+    'CG',
+    'CH',
+    'CI',
+    'CL',
+    'CM',
+    'CO',
+    'CR',
+    'CV',
+    'CW',
+    'CY',
+    'CZ',
+    'DE',
+    'DJ',
+    'DK',
+    'DM',
+    'DO',
+    'DZ',
+    'EC',
+    'EE',
+    'EG',
+    'ES',
+    'FI',
+    'FJ',
+    'FM',
+    'FR',
+    'GA',
+    'GB',
+    'GD',
+    'GE',
+    'GH',
+    'GM',
+    'GN',
+    'GQ',
+    'GR',
+    'GT',
+    'GW',
+    'GY',
+    'HK',
+    'HN',
+    'HR',
+    'HT',
+    'HU',
+    'ID',
+    'IE',
+    'IL',
+    'IN',
+    'IQ',
+    'IS',
+    'IT',
+    'JM',
+    'JO',
+    'JP',
+    'KE',
+    'KG',
+    'KH',
+    'KI',
+    'KM',
+    'KN',
+    'KR',
+    'KW',
+    'KZ',
+    'LA',
+    'LB',
+    'LC',
+    'LI',
+    'LK',
+    'LR',
+    'LS',
+    'LT',
+    'LU',
+    'LV',
+    'LY',
+    'MA',
+    'MC',
+    'MD',
+    'ME',
+    'MG',
+    'MH',
+    'MK',
+    'ML',
+    'MN',
+    'MO',
+    'MR',
+    'MT',
+    'MU',
+    'MV',
+    'MW',
+    'MX',
+    'MY',
+    'MZ',
+    'NA',
+    'NE',
+    'NG',
+    'NI',
+    'NL',
+    'NO',
+    'NP',
+    'NR',
+    'NZ',
+    'OM',
+    'PA',
+    'PE',
+    'PG',
+    'PH',
+    'PK',
+    'PL',
+    'PS',
+    'PT',
+    'PW',
+    'PY',
+    'QA',
+    'RO',
+    'RS',
+    'RU',
+    'RW',
+    'SA',
+    'SB',
+    'SC',
+    'SE',
+    'SG',
+    'SI',
+    'SK',
+    'SL',
+    'SM',
+    'SN',
+    'SR',
+    'ST',
+    'SV',
+    'SZ',
+    'TD',
+    'TG',
+    'TH',
+    'TJ',
+    'TL',
+    'TN',
+    'TO',
+    'TR',
+    'TT',
+    'TV',
+    'TW',
+    'TZ',
+    'UA',
+    'UG',
+    'US',
+    'UY',
+    'UZ',
+    'VC',
+    'VE',
+    'VN',
+    'VU',
+    'WS',
+    'XK',
+    'ZA',
+    'ZM',
+    'ZW'
+  ],
+  external_urls: {
+    spotify: 'https://open.spotify.com/album/6fQElzBNTiEMGdIeY0hy5l'
+  },
+  href: 'https://api.spotify.com/v1/albums/6fQElzBNTiEMGdIeY0hy5l',
+  id: '6fQElzBNTiEMGdIeY0hy5l',
+  images: [
+    {
+      height: 640,
+      url: 'https://i.scdn.co/image/ab67616d0000b273c657af8d545b4dcf432ace76',
+      width: 640
+    },
+    {
+      height: 300,
+      url: 'https://i.scdn.co/image/ab67616d00001e02c657af8d545b4dcf432ace76',
+      width: 300
+    },
+    {
+      height: 64,
+      url: 'https://i.scdn.co/image/ab67616d00004851c657af8d545b4dcf432ace76',
+      width: 64
+    }
+  ],
+  name: 'Hunky Dory (2015 Remaster)',
+  release_date: '1971-12-17',
+  release_date_precision: 'day',
+  total_tracks: 11,
+  type: 'album',
+  uri: 'spotify:album:6fQElzBNTiEMGdIeY0hy5l'
+}
+
+export const artistMock = {
+  external_urls: {
+    spotify: 'https://open.spotify.com/artist/0oSGxfWSnnOXhD2fKuz2Gy'
+  },
+  followers: { href: null, total: 7740929 },
+  genres: ['art rock', 'classic rock', 'glam rock', 'permanent wave', 'rock'],
+  href: 'https://api.spotify.com/v1/artists/0oSGxfWSnnOXhD2fKuz2Gy',
+  id: '0oSGxfWSnnOXhD2fKuz2Gy',
+  images: [
+    {
+      height: 640,
+      url: 'https://i.scdn.co/image/ab6761610000e5ebb78f77c5583ae99472dd4a49',
+      width: 640
+    },
+    {
+      height: 320,
+      url: 'https://i.scdn.co/image/ab67616100005174b78f77c5583ae99472dd4a49',
+      width: 320
+    },
+    {
+      height: 160,
+      url: 'https://i.scdn.co/image/ab6761610000f178b78f77c5583ae99472dd4a49',
+      width: 160
+    }
+  ],
+  name: 'David Bowie',
+  popularity: 82,
+  type: 'artist',
+  uri: 'spotify:artist:0oSGxfWSnnOXhD2fKuz2Gy'
+}
+
+export const trackMock = {
+  album: {
+    album_type: 'album',
+    artists: [
+      {
+        external_urls: {
+          spotify: 'https://open.spotify.com/artist/1dfeR4HaWDbWqFHLkxsg1d'
+        },
+        href: 'https://api.spotify.com/v1/artists/1dfeR4HaWDbWqFHLkxsg1d',
+        id: '1dfeR4HaWDbWqFHLkxsg1d',
+        name: 'Queen',
+        type: 'artist',
+        uri: 'spotify:artist:1dfeR4HaWDbWqFHLkxsg1d'
+      }
+    ],
+    available_markets: ['CA', 'US'],
+    external_urls: {
+      spotify: 'https://open.spotify.com/album/0xc5IpJM39eEEYSKDrm5kf'
+    },
+    href: 'https://api.spotify.com/v1/albums/0xc5IpJM39eEEYSKDrm5kf',
+    id: '0xc5IpJM39eEEYSKDrm5kf',
+    images: [
+      {
+        height: 640,
+        url: 'https://i.scdn.co/image/ab67616d0000b273a1e05e1048e2cf2737adf742',
+        width: 640
+      },
+      {
+        height: 300,
+        url: 'https://i.scdn.co/image/ab67616d00001e02a1e05e1048e2cf2737adf742',
+        width: 300
+      },
+      {
+        height: 64,
+        url: 'https://i.scdn.co/image/ab67616d00004851a1e05e1048e2cf2737adf742',
+        width: 64
+      }
+    ],
+    name: 'Hot Space (Deluxe Remastered Version)',
+    release_date: '1982-05-03',
+    release_date_precision: 'day',
+    total_tracks: 16,
+    type: 'album',
+    uri: 'spotify:album:0xc5IpJM39eEEYSKDrm5kf'
+  },
+  artists: [
+    {
+      external_urls: {
+        spotify: 'https://open.spotify.com/artist/1dfeR4HaWDbWqFHLkxsg1d'
+      },
+      href: 'https://api.spotify.com/v1/artists/1dfeR4HaWDbWqFHLkxsg1d',
+      id: '1dfeR4HaWDbWqFHLkxsg1d',
+      name: 'Queen',
+      type: 'artist',
+      uri: 'spotify:artist:1dfeR4HaWDbWqFHLkxsg1d'
+    },
+    {
+      external_urls: {
+        spotify: 'https://open.spotify.com/artist/0oSGxfWSnnOXhD2fKuz2Gy'
+      },
+      href: 'https://api.spotify.com/v1/artists/0oSGxfWSnnOXhD2fKuz2Gy',
+      id: '0oSGxfWSnnOXhD2fKuz2Gy',
+      name: 'David Bowie',
+      type: 'artist',
+      uri: 'spotify:artist:0oSGxfWSnnOXhD2fKuz2Gy'
+    }
+  ],
+  available_markets: ['CA', 'US'],
+  disc_number: 1,
+  duration_ms: 248440,
+  explicit: false,
+  external_ids: { isrc: 'GBUM71029622' },
+  external_urls: {
+    spotify: 'https://open.spotify.com/track/11IzgLRXV7Cgek3tEgGgjw'
+  },
+  href: 'https://api.spotify.com/v1/tracks/11IzgLRXV7Cgek3tEgGgjw',
+  id: '11IzgLRXV7Cgek3tEgGgjw',
+  is_local: false,
+  name: 'Under Pressure - Remastered 2011',
+  popularity: 71,
+  preview_url: null,
+  track_number: 11,
+  type: 'track',
+  uri: 'spotify:track:11IzgLRXV7Cgek3tEgGgjw'
+}
+
+export const resultRaw = {
+  albums: { items: [albumMock] },
+  artists: { items: [artistMock] },
+  trackMock: { items: [trackMock] }
+}
