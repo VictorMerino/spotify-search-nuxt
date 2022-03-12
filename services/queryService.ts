1a636f8d342ac1a647cc01a84a6c564a8481aa01
Victor Merino <victor.merinogarcia@gmail.com>
2022-03-12 21:44:52 +0100

    Add endpoint to get just one track

--- services/queryService.ts
+++ services/queryService.ts
@@ -24,6 +24,31 @@ export const getResultsFromQuery = async (text: string) => {
   return result
 }
 
+export const getTrack = async (trackId: string) => {
+  let result = {}
+  const json = await _requestAccessToken()
+  const token = json.token
+
+  const trackUrl = 'https://api.spotify.com/v1/tracks'
+  const getResultsEndpoint = `${trackUrl}?ids=${trackId}`
+
+  try {
+    const response = await fetch(getResultsEndpoint, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + token
+      }
+    })
+
+    result = response.ok
+      ? await response.json()
+      : { error: await response.json() }
+  } catch (error) {
+    console.log(error)
+  }
+  return result
+}
+
 const _requestAccessToken = async () => {
   return await fetch('/api/spotify')
     .then(result => {
