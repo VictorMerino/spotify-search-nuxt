a60502ed977b95fb5de092788367b61dae3f3ffe
Victor Merino <victor.merinogarcia@gmail.com>
2022-03-12 22:22:10 +0100

    Render ResultItem only when actually fetched (we need to check for Suspense component)

--- pages/track/[resultItem].vue
+++ pages/track/[resultItem].vue
@@ -6,12 +6,14 @@ import ResultItem from '@/components/ResultItem'
 const resultStore = useResultsStore()
 const resultItem = computed(() => resultStore.resultItem)
 onMounted(() => {
-  console.log('Mounted')
   resultStore.getTrack(useRoute().params.resultItem)
 })
 </script>
 <template>
   <div class="container my-4">
-    <ResultItem :item="resultItem.tracks[0]" />
+    <ResultItem
+      v-if="resultItem && resultItem.tracks"
+      :item="resultItem.tracks[0]"
+      layout="single-result" />
   </div>
 </template>
