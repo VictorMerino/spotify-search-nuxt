63e18ab25c0df9a8ad0342eb2b3687cb5c864de6
Victor Merino <victor.merinogarcia@gmail.com>
2022-03-12 21:45:58 +0100

    Render track: just the json (need to use an actual Result component)

--- pages/track/[resultItem].vue
+++ pages/track/[resultItem].vue
@@ -1,6 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useResultsStore } from '@/stores/results'
+
+const resultStore = useResultsStore()
+const resultItem = computed(() => resultStore.resultItem)
+onMounted(() => {
+  console.log('Mounted')
+  resultStore.getTrack(useRoute().params.resultItem)
+})
+</script>
 <template>
   <div class="container">
-    {{ $route.params.resultItem }}
+    <pre>{{ resultItem }}</pre>
   </div>
 </template>
