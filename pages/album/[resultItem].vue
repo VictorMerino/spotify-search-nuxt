4166e48a5f1ce7518d05fb3a8ce74b24337a443d
Victor Merino <victor.merinogarcia@gmail.com>
2022-03-13 20:57:38 +0100

    Add album and artist pages: they are replicated from track page. Needs hugh refactor: lot of duplicated code

---
+++ pages/album/[resultItem].vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { useResultsStore } from '@/stores/results'
+
+import ResultItem from '@/components/ResultItem'
+
+const resultStore = useResultsStore()
+const resultItem = computed(() => resultStore.resultItem)
+onMounted(() => {
+  resultStore.getSingleResult(useRoute().params.resultItem, 'album')
+})
+</script>
+<template>
+  <div class="container my-4">
+    <ResultItem v-if="resultItem" :item="resultItem" layout="single-result" />
+  </div>
+</template>
