37558195af78a783170811384c598153d6d90ef2
Victor Merino <victor.merinogarcia@gmail.com>
2022-03-13 20:56:51 +0100

    Now it's simpler: we don't need several tracks, so result is a cleaner object

--- pages/track/[resultItem].vue
+++ pages/track/[resultItem].vue
@@ -6,14 +6,11 @@ import ResultItem from '@/components/ResultItem'
 const resultStore = useResultsStore()
 const resultItem = computed(() => resultStore.resultItem)
 onMounted(() => {
-  resultStore.getTrack(useRoute().params.resultItem)
+  resultStore.getSingleResult(useRoute().params.resultItem, 'track')
 })
 </script>
 <template>
   <div class="container my-4">
-    <ResultItem
-      v-if="resultItem && resultItem.tracks"
-      :item="resultItem.tracks[0]"
-      layout="single-result" />
+    <ResultItem v-if="resultItem" :item="resultItem" layout="single-result" />
   </div>
 </template>
